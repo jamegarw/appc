79a4b1f67eeea55b53c7bbc784b0ad2b24899c3a
bluemixtraining1 <bluemixtraining1@gmail.com>
2015-09-28 23:38:21 -0700

    my changes

--- src/main/java/appc/ServiceC.java
+++ src/main/java/appc/ServiceC.java
@@ -26,9 +26,9 @@ public class ServiceC implements IServiceC {
 	
 	@Override
 	public int jumble(int leftOne, int rightOne, int leftTwo, int rightTwo) {
-		String serviceCclientId = "9aa19969-286b-43a5-9e04-4069530e6560";
-
-		String url = "https://api.apim.ibmcloud.com/bluemixtraininganzgmailcom-dev/sb";
+		String serviceCclientId = "16dd567e-548b-4f9f-8444-87b6ef2b7502";
+		
+		String url = "https://api.apim.ibmcloud.com/jamegarwau1ibmcom-dev/sb";
 		
 		Map<String, List<String>> serviceARequestHeaders = new HashMap<String, List<String>>();
 		serviceARequestHeaders.put("X-IBM-Client-Id", Collections.singletonList(serviceCclientId));
